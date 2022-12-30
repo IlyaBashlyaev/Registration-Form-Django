8bccf7dee1b3847b1d74023f1bae455910bafaf2
Ilya Bashlyaev <87471648+IlyaBashlyaev@users.noreply.github.com>
2022-12-30 11:27:12 +0100

    Commit

--- Registration/settings.py
+++ Registration/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w9e##@snjfk4@krjg%^c99qyy8*9pg&i3t__!&z^&k2=q=%!is'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
